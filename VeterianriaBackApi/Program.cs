064b92510d145a308e2469c5d316876fc9d658fb
Ranicaro <nicolascaceresrom@hotmail.com>
2023-07-16 10:10:53 -0500

    cors v2

--- VeterianriaBackApi/Program.cs
+++ VeterianriaBackApi/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -43,5 +44,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors("corsapp");
+app.UseCors("CorsPolicy");
 app.Run();
